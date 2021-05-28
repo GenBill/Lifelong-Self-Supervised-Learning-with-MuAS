21bbf946e9ec91009a307e7baec433ab0e4003af
Bill Zhang <30310636+GenBill@users.noreply.github.com>
2021-05-28 09:22:47 +0800

    Frost

--- Frost_Func.py
+++ Frost_Func.py
@@ -15,9 +15,27 @@ def Get_BW_net(net, device):
     print("Shadow Run !")
     return shadow_net
 
-def Frost_iter(net, shadow_net, X, target, loss_Func, optimizer):
+def params_diff(net, old_net):
+    # zero part of _grad
+    params_net = list(net.named_parameters())
+    params_old = list(old_net.named_parameters())
+    loss = 0
+    for i in range(len(params_net)):
+        loss += sum(abs(params_net[i][1].data - params_old[i][1].data))
+    return loss
+
+def Frost_iter(net, shadow_net, old_net, X, target, loss_Func, optimizer, reg_lamb=5):
+    # 计算基本损失
     pred = net(X)
     loss = loss_Func(pred, target)
+
+    # 添加遗忘损失
+    params_net = list(net.named_parameters())
+    params_old = list(old_net.named_parameters())
+    for i in range(len(params_net)):
+        loss += reg_lamb * sum(abs(params_net[i][1].data - params_old[i][1].data))
+    
+    # 计算 grad
     optimizer.zero_grad()
     loss.backward()
 
@@ -29,4 +47,7 @@ def Frost_iter(net, shadow_net, X, target, loss_Func, optimizer):
         params_net[i][1].grad = params_net[i][1].grad * params_shadow[i][1].data.float()
         print(params_net[i][1].grad)    # data
 
+    # 
     optimizer.step()
+
+    # 明天用简单的Net写个测试

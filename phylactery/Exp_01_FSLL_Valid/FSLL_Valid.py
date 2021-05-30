539310ab0fd32d6a18e5328fcc9feafb35c3ddf4
Bill Zhang <30310636+GenBill@users.noreply.github.com>
2021-05-30 10:29:32 +0800

    eLich

--- phylactery/Exp_01_FSLL_Valid/FSLL_Valid.py
+++ phylactery/Exp_01_FSLL_Valid/FSLL_Valid.py
@@ -5,8 +5,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from torchvision import datasets, models
+from torchvision import transforms, utils, datasets, models
 from torch.utils.data import Dataset, DataLoader
+
+# 过滤警告信息
+import warnings
+warnings.filterwarnings("ignore")
 
 # copy from Frost_Func.py
 # Sector Frost_Func
@@ -65,6 +69,10 @@ def Frost_iter(net, shadow_net, old_net, X, target, loss_Func, optimizer, reg_la
     
     # End Sub
     optimizer.step()
+    acc_mat = torch.argmax(pred, 1)==torch.argmax(target, 1)
+    acc_time = torch.sum(acc_mat)
+
+    return loss.item(), acc_time.item()
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -72,30 +80,43 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # 任务：CIFAR10，CUB200（暂定）
 # ResNet-18 装载
+this_Epoch = 0
+checkpoint_path = 'E:/Laplace/Dataset/Kaggle265/models' # './TimHu/models'
 
+Trainset_path = 'E:/Laplace/Dataset/Kaggle265/train'    # './Kaggle265/train'
+Testset_path = 'E:/Laplace/Dataset/Kaggle265/test'      # './Kaggle265/test'
+Validset_path = 'E:/Laplace/Dataset/Kaggle265/valid'    # './Kaggle265/valid'
+
+batch_size = 64
+num_epochs = 200
 
 # 载入数据
-class RotationDataset(Dataset):
 
-    def __init__(self, data_root_dir, preTransform=None, postTransform=None):
-        
-        self.data_root_dir = data_root_dir
-        # Output of pretransform should be PIL images
-        self.preTransform = preTransform
-
-        self.postTransform = postTransform
-
-        self.dataset = datasets.ImageFolder(self.data_root_dir, self.preTransform)
-
-    def __len__(self):
-        return len(self.dataset)
+data_transform = transforms.Compose([
+    # transforms.Resize(224),           # 缩放图片(Image)，保持长宽比不变，最短边为32像素
+    # transforms.CenterCrop(224),       # 从图片中间切出32*32的图片
+    transforms.ToTensor(),              # 将图片(Image)转成Tensor，归一化至[0, 1]
+    transforms.Normalize(
+        mean = [0.492, 0.461, 0.417], 
+        std = [0.256, 0.248, 0.251]
+    )                                   # 标准化至[-1, 1]，规定均值和标准差
+])
 
 
+data_train = datasets.ImageFolder(root = Trainset_path, transform = data_transform)
 
 # 就是普通的载入数据
+data_train = datasets.ImageFolder(root = Trainset_path, transform = data_transform)
+train_loader = torch.utils.data.DataLoader(data_train, batch_size=batch_size, shuffle=True)
+
+
 
 # 载入模型
 net = models.resnet50(pretrained=True)
+
+if this_Epoch != 0 :
+    net.load_state_dict(torch.load('%s/Epoch_%d.pth' % (checkpoint_path, this_Epoch), map_location=device))
+
 # 就是普通的载入模型
 optimizer = optim.SGD(
     [
@@ -103,6 +124,8 @@ optimizer = optim.SGD(
     ]   , lr=1e-1, momentum=0.8, weight_decay=0.0001
 )
 criterion = nn.MSELoss(reduction='mean')
+loss_list = []
+accrate_list = []
 
 # Train Step
 # 镜像
@@ -110,6 +133,19 @@ old_net = Get_old_net(net)
 shadow_net = Get_shadow_net(net, 2)
 
 # Epoch On !
-X_sample = torch.rand(1,2).float().to(device)
-Y_sample = torch.rand(1,1).float().to(device)
-Frost_iter(net, shadow_net, old_net, X_sample, Y_sample, criterion, optimizer, reg_lamb=1e-2)
+for epoch in tqdm(range(1,num_epochs)):
+
+    for batch_num, (inputs, labels) in enumerate(train_loader):
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        this_loss, this_acc = Frost_iter(net, shadow_net, old_net, inputs, labels, criterion, optimizer, reg_lamb=1e-2)
+        loss_list.append(this_loss)
+        accrate_list.append(this_acc/batch_size)
+
+        if epoch % 10 == 0 :
+            print('Epoch:', epoch, ', acc:', this_acc/batch_size, ', loss:', this_loss)
+        if epoch % 1000 == 0 :
+            torch.save(net.state_dict(), '%s/Epoch_%d.pth' % (checkpoint_path, this_Epoch+epoch))
+
+

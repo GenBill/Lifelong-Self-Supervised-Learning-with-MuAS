0dbea4b200213eefa4e4141138225c1b899e198a
GenBill <zhang478127826@gmail.com>
2021-06-14 15:03:59 +0800

    Boom

---
+++ Big_Joint.sh
@@ -0,0 +1,5 @@
+python main_joint.py --cuda '0,1' --batchsize 512 --numworkers 2  --joint 4 --pretrain 0
+python main_joint.py --cuda '2,3' --batchsize 512 --numworkers 2  --joint 4 --pretrain 1
+
+python main_joint.py --cuda '0,1' --batchsize 256 --numworkers 2  --joint 4 --pretrain 0
+python main_joint.py --cuda '2,3' --batchsize 256 --numworkers 2  --joint 4 --pretrain 1

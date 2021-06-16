58fc6f340931d11c05bd5bd4ad1097dbace5c884
GenBill <zhang478127826@gmail.com>
2021-06-16 11:46:17 +0800

    Mercy

--- Run_Joint.sh
+++ Run_Joint.sh
@@ -1,12 +1,12 @@
 python main_joint.py --cuda '0' --batchsize 256 --numworkers 2  --joint 0 --pretrain 0 &
-python main_joint.py --cuda '1' --batchsize 256 --numworkers 2  --joint 0 --pretrain 1 &
-python main_joint.py --cuda '2' --batchsize 256 --numworkers 2  --joint 1 --pretrain 0 &
-python main_joint.py --cuda '3' --batchsize 256 --numworkers 2  --joint 1 --pretrain 1
+python main_joint.py --cuda '1' --batchsize 256 --numworkers 2  --joint 0 --pretrain 1
+python main_joint.py --cuda '0' --batchsize 256 --numworkers 2  --joint 1 --pretrain 0 &
+python main_joint.py --cuda '1' --batchsize 256 --numworkers 2  --joint 1 --pretrain 1
 
 python main_joint.py --cuda '0' --batchsize 256 --numworkers 2  --joint 2 --pretrain 0 &
-python main_joint.py --cuda '1' --batchsize 256 --numworkers 2  --joint 2 --pretrain 1 &
-python main_joint.py --cuda '2' --batchsize 256 --numworkers 2  --joint 3 --pretrain 0 &
-python main_joint.py --cuda '3' --batchsize 256 --numworkers 2  --joint 3 --pretrain 1
+python main_joint.py --cuda '1' --batchsize 256 --numworkers 2  --joint 2 --pretrain 1
+python main_joint.py --cuda '0' --batchsize 256 --numworkers 2  --joint 3 --pretrain 0 &
+python main_joint.py --cuda '1' --batchsize 256 --numworkers 2  --joint 3 --pretrain 1
 
 python main_joint.py --cuda '0,1' --batchsize 256 --numworkers 2  --joint 4 --pretrain 0
-python main_joint.py --cuda '2,3' --batchsize 256 --numworkers 2  --joint 4 --pretrain 1
+python main_joint.py --cuda '0,1' --batchsize 256 --numworkers 2  --joint 4 --pretrain 1

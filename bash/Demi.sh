f1c649169b819a38df25e1b43b7bfd83cd1962e6
GenBill <zhang478127826@gmail.com>
2021-06-19 21:57:18 +0800

    Update Bash

--- bash/Demi.sh
+++ bash/Demi.sh
@@ -3,9 +3,9 @@
 
 
 # On Tesla_V100
-python main_demi.py --cuda '4,5,6,7' --batchsize 128 --numworkers 2 --pretrain 0 --lr_net 0.0001 --lr_fc 0.0001 --epochs_0 400 --epochs_1 80 &
-python main_demi.py --cuda '1' --batchsize 256 --numworkers 2 --pretrain 0 --lr_net 0.0001 --lr_fc 0.0001 --epochs_0 400 --epochs_1 80 &
-python main_demi.py --cuda '2' --batchsize 256 --numworkers 4 --pretrain 1 --lr_net 0.0001 --lr_fc 0.0001 --epochs_0 400 --epochs_1 80 &
+python main_demi.py --cuda '4,5,6,7' --batchsize 256 --numworkers 2 --pretrain 0 --lr_net 0.0001 --lr_fc 0.0001 --epochs_0 400 --epochs_1 80 &
+python main_demi.py --cuda '1' --batchsize 256 --numworkers 2 --pretrain 1 --lr_net 0.0001 --lr_fc 0.0001 --epochs_0 400 --epochs_1 80 &
+python main_demi.py --cuda '2' --batchsize 256 --numworkers 2 --pretrain 0 --lr_net 0.0001 --lr_fc 0.0001 --epochs_0 400 --epochs_1 80 &
 
 
 # On RTX3090

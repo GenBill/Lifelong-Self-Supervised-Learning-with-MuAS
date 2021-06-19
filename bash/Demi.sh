b58660401ca60f0030bb6b8ff1fbbf1a11514fc7
GenBill <zhang478127826@gmail.com>
2021-06-19 21:51:13 +0800

    Update Bash

--- bash/Demi.sh
+++ bash/Demi.sh
@@ -1,11 +1,13 @@
+# subs python ../main_demi.py
+# Use lr = 1e-4
+
+
 # On Tesla_V100
-# python ../main_demi.py
-python main_demi.py --cuda '0' --batchsize 256 --numworkers 2 --pretrain 0 --lr_net 0.001 --lr_fc 0.001 --epochs_0 400 --epochs_1 80 &
-python main_demi.py --cuda '1' --batchsize 256 --numworkers 2 --pretrain 0 --lr_net 0.001 --lr_fc 0.002 --epochs_0 400 --epochs_1 80 &
-python main_demi.py --cuda '2' --batchsize 256 --numworkers 4 --pretrain 0 --lr_net 0.005 --lr_fc 0.01 --epochs_0 400 --epochs_1 80 &
+python main_demi.py --cuda '4,5,6,7' --batchsize 128 --numworkers 2 --pretrain 0 --lr_net 0.0001 --lr_fc 0.0001 --epochs_0 400 --epochs_1 80 &
+python main_demi.py --cuda '1' --batchsize 256 --numworkers 2 --pretrain 0 --lr_net 0.0001 --lr_fc 0.0001 --epochs_0 400 --epochs_1 80 &
+python main_demi.py --cuda '2' --batchsize 256 --numworkers 4 --pretrain 1 --lr_net 0.0001 --lr_fc 0.0001 --epochs_0 400 --epochs_1 80 &
+
 
 # On RTX3090
 python main_demi.py --cuda '1' --batchsize 256 --numworkers 4 --pretrain 1 --lr_net 0.005 --lr_fc 0.01 --epochs_0 400 --epochs_1 80
 
-# CPU 代码测试
-# python main_demi.py --cuda '' --batchsize 16 --numworkers 2 --pretrain 0 --epochs_0 2 --epochs_1 2
